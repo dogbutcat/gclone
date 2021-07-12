de3e99f082d63c60f3d315dde61a1a4faef48439
dogbutcat <dogbutcat@hotmail.com>
2021-07-12 13:56:40 +0800

    compile: original version tag overwrite drop

--- gclone.go
+++ gclone.go
@@ -5,11 +5,9 @@ import (
 	"github.com/rclone/rclone/cmd"
 	_ "github.com/dogbutcat/gclone/cmd/copy"
 	_ "github.com/rclone/rclone/cmd/all"    // import all commands
-	"github.com/rclone/rclone/fs"
 	_ "github.com/rclone/rclone/lib/plugin" // import plugins
 )
 
 func main() {
-	fs.Version = fs.Version+"-mod1.3.1"
 	cmd.Main()
 }
