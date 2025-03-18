b3294473c797c6af4a9c34eab960ccae6707d5aa
dogbutcat <dogbutcat@hotmail.com>
2025-03-18 11:13:14 +0800

    update: add comment for extra file

--- bin/tools.go
+++ bin/tools.go
@@ -1,3 +1,6 @@
+// This file is no meaningful except for mod tidy keeping those indirect mod(s) avoid to be deleted.
+// those mod(s) is relevant with build action part, no infurence with local build or functionality
+// DO NOT remove those if needing github-action work.
 package main
 
 import (
